fd8bc83b137a4f828cffdf331041a23909d25165
yuvaraj-1991 <yuviatudemy@gmail.com>
2026-07-29 23:30:48 +0530

    server search & add function added

--- server_manager.py
+++ server_manager.py
@@ -1,6 +1,6 @@
 #Here we will start with Server Configuration 
 
-import json 
+
 
 # with open('servers.json', 'r') as file:
 #     servers = json.load(file)
@@ -13,6 +13,8 @@ import json
 #         for key,value in server.items():
 #             print(f"{key} : {value}")
 
+
+import json 
 
 #Function to load servers
 
@@ -33,7 +35,9 @@ servers = load_servers()
 display_servers(servers)
 
 def search_servers(servers):
-    findServer = int(input("Enter the Server ID : "))
+    print("-"*50)
+    print("-"*50)
+    findServer = int(input("Enter the Server ID : "))    
     for server in servers:
         if server["ServerID"] == findServer:
             print("Server found! ")
@@ -42,6 +46,50 @@ def search_servers(servers):
                 print(f"{key} : {value}")
             break
     else:
-        print("Server not found!")          
+        print("Server not found!")      
 
-          
+search_servers(servers)            
+
+def add_servers(servers):
+
+    new_server = {}
+
+    while True:
+
+        server_id = int(input("Enter the New Server ID : "))
+
+        for server in servers:
+            if server['ServerID'] == server_id:
+               print('Server ID already exists! Please enter again unique ID : ')
+               break
+        else:
+            new_server['ServerID'] = server_id
+
+                   
+            host_name = input("Enter the HostName : ")
+            new_server['Hostname'] = host_name
+
+            ip_add = input("Enter the IP Address : ")
+            new_server['IPAddress'] = ip_add
+
+            environment = input("Enter the Environment QA/Dev/Prod : ")
+            new_server['Environment'] = environment
+
+            os_sys = input("Enter the Operating System : ")
+            new_server['OperatingSystem'] = os_sys
+
+            status = input("Enter the Status Run/Stopped/Paused : ")
+            new_server['Status'] = status
+
+            servers.append(new_server) #appending it to the list        
+
+            print("Server Added Successfully! ")
+
+            with open('servers.json','w') as fil:
+                json.dump(servers,fil, indent=4)
+
+            break     
+
+        
+add_servers(servers)             
+
