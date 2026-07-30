2165af698b9795383656b882c83103bd34a2de0e
yuvaraj-1991 <yuviatudemy@gmail.com>
2026-07-30 15:44:32 +0530

    editing main.py adding userchoice and menu

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 
 import json 
 
+with open('servers.json', 'r') as file:
+    servers = json.load(file)
+
 while True: 
 
         print(''' 
@@ -18,7 +21,20 @@ while True:
 
                 ''')
 
-        user_choice = int(input("Enter your Choice : "))
+        user_choice = int(input("Enter your Choice : "))        
+
+        if user_choice == 1:
+                display_servers(servers)
+        elif user_choice == 2:
+                search_servers(servers)    
+        elif user_choice == 3:
+                add_servers(servers)  
+        elif user_choice == 4:
+                update_servers(servers)  
+        elif user_choice == 5:
+                delete_servers(servers) 
+        elif user_choice == 6:
+                print("Exiting!! Have a Nice Day! ")                               
 
 
-        break
+        
